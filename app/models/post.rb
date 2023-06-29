7f80f2b20a87d95772b48d7577685042c9ed037d
ned <nedschultz@outlook.com>
2023-06-29 16:36:03 +1000

    commit tree 3a03d5d2d7a98e24ea6037840e0e104e56d9c48e

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+
+  validate :sufficiently_clickbait_title
+
+  def sufficiently_clickbait_title
+    unless title&.match?(/Won't Believe|Secret|Top \d+|Guess/)
+      errors.add(:title, "must be sufficiently clickbait-y")
+    end
+  end
 end
